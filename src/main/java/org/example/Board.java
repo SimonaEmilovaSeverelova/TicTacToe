1334d992fa7c533f68c6ebdcf3c9348df7ece965
Simona <simona.severelova@gmail.com>
2025-06-26 15:28:00 +0200

    US2 Board implementiert: place() und print() Methode

---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == ' ';
+    }
+
+    public void place(int x, int y, char marker) {
+        cells[x][y] = marker;
+    }
+
+    public boolean isFull() {
+        for (char[] row : cells) {
+            for (char cell : row) {
+                if (cell == ' ') return false;
+            }
+        }
+        return true;
+    }
+
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print() {
+        for (char[] row : cells) {
+            for (char cell : row) {
+                System.out.print("|" + cell);
+            }
+            System.out.println("|");
+        }
+    }
+}
